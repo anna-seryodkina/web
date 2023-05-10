47d5afe02242fa9bc065cdc7f138e897b0dc16a3
anna-seryodkina <a.serodkina@gmail.com>
2023-05-11 01:16:19 +0300

    db fix

--- backend/src/db/mongo.mjs
+++ backend/src/db/mongo.mjs
@@ -1,17 +1,17 @@
 import { MongoClient } from "mongodb";
 
 const url = 'mongodb://localhost:27017';
+const dbName = 'local';
 
 const client = new MongoClient(url);
 
-let connection;
 
 try{
-    connection = await client.connect();
+    await client.connect();
 } catch (e){
     console.error(e);
 }
 
-const db = connection.db('local');
+const db = client.db(dbName);
 
 export default db;
